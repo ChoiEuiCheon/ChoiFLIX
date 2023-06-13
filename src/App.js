45a00ace39687a3b8205cf80b8ad562f57f4e3b7
choieuicheon <choieuicheon@naver.com>
2023-06-13 17:18:48 +0900

    23.06.13

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
   );
 }
 
