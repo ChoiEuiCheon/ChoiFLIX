90b06100f585b8571634e0ab237425ca25a9585f
choieuicheon <choieuicheon@naver.com>
2023-06-15 17:35:53 +0900

    Updates

---
+++ precache-manifest.7288b61fc6607874eaa9418a4f3b35fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba992632c84ab56a3105b2f6ebee041",
+    "url": "/ChoiFLIX/index.html"
+  },
+  {
+    "revision": "915da3ae01ea399b3b39",
+    "url": "/ChoiFLIX/static/css/main.9826f6b4.chunk.css"
+  },
+  {
+    "revision": "26367bb5ade769909a8a",
+    "url": "/ChoiFLIX/static/js/2.3e54bc0a.chunk.js"
+  },
+  {
+    "revision": "915da3ae01ea399b3b39",
+    "url": "/ChoiFLIX/static/js/main.5035f022.chunk.js"
+  },
+  {
+    "revision": "e0cc8f8e6bd29538147d",
+    "url": "/ChoiFLIX/static/js/runtime~main.c0fd7d89.js"
+  }
+]);
